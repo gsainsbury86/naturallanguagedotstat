9e7824b48696a40f2360d04e0f557e61807f7204
gsainsbury86 <gsainsbury86@gmail.com>
2014-07-21 21:33:21 +0930

    Fixed posts?

--- src/main/java/naturallanguagedotstat/Service.java
+++ src/main/java/naturallanguagedotstat/Service.java
@@ -24,6 +24,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -69,8 +70,8 @@ public class Service {
 
 	@POST
 	@Path("/query")
-	@Consumes("application/json")
-	@Produces("application/json")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
 	public Response query(String query) throws SQLException{
 
 		if(!LocalTest.unitTests){
--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -5,41 +5,48 @@ $(document).ready(function() {
 		}
 	});
 
-	$.get('/main/randomQuery', function(data){
+	$.get('main/randomQuery', function(data){
 		$('#query').attr('placeholder',data);
 	});
 
 	$('#go').click( function() {
 		$('#result').text('searching...');
-		var url = '/main/query';
-		var data = {'"query"':$('#query').val()};
-		$.post(url, data, function(data, status, jqXHR) {
-			if (status == "error"){
-				$('#result').text(xhr.status + " " + xhr.statusText);
-			}
-			else{
-				$("#result").text(data.result.toString().replace(/\B(?=(\d{3})+(?!\d))/g,","));
-				$("#url").html('URL : ' + '<a href="'+data.url+'" target="_blank" >' + 'source' + '</a><br>');
-				$("#datasetURL").html('<a href="'+data.datasetURL+'" target="_blank" >' + data.datasetName + '</a><br>');
-				$('#map').text(' ');
-				$.each(	data, function(index) {
-					if (index != 'result' && index != 'url' && index != 'datasetName' && index != 'datasetURL') {
-						var e = data[index];
+		var url = 'main/query';
+		var data = {query:$('#query').val()};
+		$.ajax({
+			url:url,
+			type:"POST",
+			data:JSON.stringify(data),
+			contentType:"application/json",
+			dataType:"json",
+			success: function(data, status, jqXHR) {
+				if (status == "error"){
+					$('#result').text(xhr.status + " " + xhr.statusText);
+				}
+				else{
+					$("#result").text(data.result.toString().replace(/\B(?=(\d{3})+(?!\d))/g,","));
+					$("#url").html('URL : ' + '<a href="'+data.url+'" target="_blank" >' + 'source' + '</a><br>');
+					$("#datasetURL").html('<a href="'+data.datasetURL+'" target="_blank" >' + data.datasetName + '</a><br>');
+					$('#map').text(' ');
+					$.each(	data, function(index) {
+						if (index != 'result' && index != 'url' && index != 'datasetName' && index != 'datasetURL') {
+							var e = data[index];
 
-						/*TODO: Fix for age ranges*/
-						if ($.isArray(e) && e.length > 1 && index == "Age") {
-							var dataArray = [];
-							for (year in e) {
-								var word = e[year];
-								dataArray.push(word);
+							/*TODO: Fix for age ranges*/
+							if ($.isArray(e) && e.length > 1 && index == "Age") {
+								var dataArray = [];
+								for (year in e) {
+									var word = e[year];
+									dataArray.push(word);
+								}
+								e = dataArray.sort(function(a,b) { return a	- b;});
+								e = e[0] + ", ... , " + e[e.length - 1];
 							}
-							e = dataArray.sort(function(a,b) { return a	- b;});
-							e = e[0] + ", ... , " + e[e.length - 1];
+							$("#map").append(index + " : " + e + '<br>');
 						}
-						$("#map").append(index + " : " + e + '<br>');
-					}
-				});
+					});
+				}
 			}
-		}, 'json');
+		});
 	});
 });
