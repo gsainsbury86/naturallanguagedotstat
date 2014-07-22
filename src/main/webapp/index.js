26217180e16cbfccbf5f9d55ca2eb733b2216baf
gsainsbury86 <gsainsbury86@gmail.com>
2014-07-22 17:18:14 +0930

    Another force push... I don't know why it doesn't like updating just script files

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -13,7 +13,6 @@ $(document).ready(function() {
 			var queryi = data.queries[i];
 			list.append('<li><a onclick="'+ "$('#query').val('"+queryi.replace(/(['"])/g, "\\$1")+"'); $('#go').click();"  +'" href="#">'+queryi+'</a></li>');				
 		}
-
 		$('#myCanvas').tagcanvas({
 			textColour: '#000000',
 			outlineColour: '#ffffff',
