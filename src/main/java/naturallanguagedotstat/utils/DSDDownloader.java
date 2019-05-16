397d0d87f61df2a0bfbe7884bc37fa3f8d206732
George Sainsbury <george.sainsbury@gmail.com>
2019-05-16 17:06:35 +0930

    tidied up unused resources

--- src/main/java/naturallanguagedotstat/utils/DSDDownloader.java
+++ src/main/java/naturallanguagedotstat/utils/DSDDownloader.java
@@ -22,30 +22,30 @@ import naturallanguagedotstat.model.Dimension;
  *
  */
 public class DSDDownloader {
-	
+
 	private static final String RESOURCES = "src/main/webapp/WEB-INF/resources/";
 	static HashMap<String,String> ASGS2011CodeList;
 	static String serverName = "stat.abs.gov.au";
 	static String datasetName = "ABS_CENSUS2011";
 	static ArrayList<Dataset> datasets;
 	static int NUM_COLLECTIONS = 28;
-	
-	
+
+
 	//TODO: http://stat.data.abs.gov.au/restsdmx/sdmx.ashx/GetDataStructure/ABS_C16_T01_LGA
 	// 		http://stat.data.abs.gov.au/restsdmx/sdmx.ashx/GetDataStructure/ABS_C16_T01_SA
 	// 		28 datasets
 
 
 	public static void main(String[] args) throws IOException{
-		
 
-//		for(int i = 1; i <= 46; i++){
-//		String dsNumber = Utils.intToString(i,2);
-//			PrintWriter writer = new PrintWriter(RESOURCES+dsNumber+".xml", "UTF-8");
-//			writer.println(Utils.httpGET("http://"+serverName+"/restsdmx/sdmx.ashx/GetDataStructure/ABS_CENSUS2011_B"+Utils.intToString(i, 2)+"/ABS"));
-//			writer.close();
-//
-//		}
+
+		//		for(int i = 1; i <= 46; i++){
+		//		String dsNumber = Utils.intToString(i,2);
+		//			PrintWriter writer = new PrintWriter(RESOURCES+dsNumber+".xml", "UTF-8");
+		//			writer.println(Utils.httpGET("http://"+serverName+"/restsdmx/sdmx.ashx/GetDataStructure/ABS_CENSUS2011_B"+Utils.intToString(i, 2)+"/ABS"));
+		//			writer.close();
+		//
+		//		}
 
 
 		datasets = new ArrayList<Dataset>();
@@ -57,61 +57,61 @@ public class DSDDownloader {
 			//BufferedReader br = new BufferedReader(fr);
 			//String line = br.readLine();
 			//br.close();
-			
-			 InputStream in = new URL( "http://stat.data.abs.gov.au/restsdmx/sdmx.ashx/GetDataStructure/ABS_C16_T01_SA" ).openStream();
 
-			 try {
-				 String line = IOUtils.toString( in );
-				 System.out.println(line);
+			InputStream in = new URL( "http://stat.data.abs.gov.au/restsdmx/sdmx.ashx/GetDataStructure/ABS_C16_T01_SA" ).openStream();
 
-			
-			
-			
-			Document doc = Utils.XMLToDocument(line);
+			try {
+				String line = IOUtils.toString( in );
+				System.out.println(line);
 
-			Dataset dataset = new Dataset(datasetName+"_B"+dsNumber,null);
 
-			ArrayList<String> dimensionNameList = findDimensions(doc);
 
-			ArrayList<Dimension> dimensions = new ArrayList<Dimension>();
 
-			for(String dimName : dimensionNameList){
-//				HashMap<String,String> map = Utils.findCodeLists(doc, dimName);
-				HashMap<String, String> map = null;
-				String dimLabel = findDimLabel(doc, dimName);
-				Dimension dim = new Dimension(dimLabel,dimName, map);
-				dimensions.add(dim);
+				Document doc = Utils.XMLToDocument(line);
+
+				Dataset dataset = new Dataset(datasetName+"_B"+dsNumber,null);
+
+				ArrayList<String> dimensionNameList = findDimensions(doc);
+
+				ArrayList<Dimension> dimensions = new ArrayList<Dimension>();
+
+				for(String dimName : dimensionNameList){
+					//				HashMap<String,String> map = Utils.findCodeLists(doc, dimName);
+					HashMap<String, String> map = null;
+					String dimLabel = findDimLabel(doc, dimName);
+					Dimension dim = new Dimension(dimLabel,dimName, map);
+					dimensions.add(dim);
+				}
+
+				dataset.setDimensions(dimensions);
+				datasets.add(dataset);
+			} finally {
+				IOUtils.closeQuietly(in);
 			}
-
-			dataset.setDimensions(dimensions);
-			datasets.add(dataset);
-			 } finally {
-				   IOUtils.closeQuietly(in);
-				 }
 		}
-		
+
 		FileOutputStream fileOut = new FileOutputStream(RESOURCES+"/dataset_summaries.ser");
 		ObjectOutputStream out = new ObjectOutputStream(fileOut);
 		out.writeObject(datasets);
 		out.close();
 		fileOut.close();
-		
-//		ArrayList<Dimension> dims = datasets.get(0).getDimensions();
-//		for(Dimension dim : dims){
-//			if(dim.getName().equals("Region")){
-//				ASGS2011 = dim;
-//			}
-//		}
-		
-//		FileOutputStream fileOut = new FileOutputStream("src/main/webapp/DSDs/ASGS_2011.ser");
-//		ObjectOutputStream out = new ObjectOutputStream(fileOut);
-//		out.writeObject(ASGS2011);
-//		out.close();
-//		fileOut.close();
+
+		//		ArrayList<Dimension> dims = datasets.get(0).getDimensions();
+		//		for(Dimension dim : dims){
+		//			if(dim.getName().equals("Region")){
+		//				ASGS2011 = dim;
+		//			}
+		//		}
+
+		//		FileOutputStream fileOut = new FileOutputStream("src/main/webapp/DSDs/ASGS_2011.ser");
+		//		ObjectOutputStream out = new ObjectOutputStream(fileOut);
+		//		out.writeObject(ASGS2011);
+		//		out.close();
+		//		fileOut.close();
 
 
 	}
-	
+
 	public static String findDimLabel(Document doc, String dim) {
 		//HashMap<String,String> dimensions = new HashMap<String,String>();
 
@@ -146,5 +146,5 @@ public class DSDDownloader {
 		}
 		return dimensions;
 	}
-	
+
 }
